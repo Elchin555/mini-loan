f37488da58babb32177c72e47925e5c72a533041
elchin.rahimli <elchinr@maestro.st>
2024-05-23 18:34:59 +0200

    Add Vue Router 4 setup

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="bg-red-400">
-    mini Loan
+  <div>
+  <router-view></router-view>
+
   </div>
-  
 </template>
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  { path: '/',
+   component: () => import('../views/HomeView.vue'),
+   name:"Home"
+   },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
