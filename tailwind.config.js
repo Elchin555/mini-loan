c7760e1ce0df87c297154c2c87eeb6557050a828
elchin.rahimli <elchinr@maestro.st>
2024-05-24 00:51:00 +0200

    add custom colors to Tailwind config and created TheHeader component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  content: [],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-bg': '#E3D2FF',
+        'secondary-bg': '#FBFBF9',
+        'secondary-bg-dark': '#1F1B34',
+        'primary-text': '#21093A',
+        'primary-btn': '#AA93FF',
+        'btn-enabled': '#AA93FF',
+        'btn-hovered': '#BBA8FF',
+        'btn-focused': '#BBA8FF',
+        'btn-pressed': '#9F85FF',
+        'btn-disabled': '#DEDEDE',
+      },
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['hover', 'focus', 'active', 'disabled'],
+    },
   },
   plugins: [],
 }
