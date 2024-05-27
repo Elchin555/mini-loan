5fd466865b4ac65a114e2f130931eb26f2f0a0b7
elchin.rahimli <elchinr@maestro.st>
2024-05-27 02:00:59 +0200

    created not-found-view

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,7 +1,30 @@
-<script lang="ts" setup>
+<script setup>
+import ResultCard from '@/components/ResultCard.vue';
+import {useLoanStore} from "@/stores/loanStore"
 
+const loanStore=useLoanStore()
+
+const user = {
+  firstName: loanStore.userName,
+}
+
+const result = {
+  message: {
+    label: `Page missing`,
+    description: 'We couldn’t find the page you’re looking for.',
+  },
+  link: {
+    name: 'home',
+    label: 'Go to home page'
+  },
+  isPositive: false
+}
 </script>
 
 <template>
-  <div>Not Found Page</div>
+  <div class="p-4 bg-white md:bg-secondary-bg">
+    <ResultCard :result="result">
+      <img class=" max-h-48" src="@/assets/sad-document.svg" alt="">
+    </ResultCard>
+  </div>
 </template>
